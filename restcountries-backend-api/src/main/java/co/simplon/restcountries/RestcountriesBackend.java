6311c28c7ada7b5bfd478cd49e5d0eb5b0b2c85c
Pascal Khuu <pascal.khuu@orange.fr>
2022-04-12 16:37:59 +0200

    initialisation of restcountries-backend-api and restcountries-backend-db

---
+++ restcountries-backend-api/src/main/java/co/simplon/restcountries/RestcountriesBackend.java
@@ -0,0 +1,13 @@
+package co.simplon.restcountries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestcountriesBackend {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestcountriesBackend.class, args);
+	}
+
+}
