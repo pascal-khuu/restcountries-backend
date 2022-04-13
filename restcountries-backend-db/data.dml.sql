f15b7e8daec71334be1e07b098469d7342e11c45
Pascal Khuu <pascal.khuu@orange.fr>
2022-04-13 16:49:08 +0200

    order insertion data countries updated

--- restcountries-backend-db/data.dml.sql
+++ restcountries-backend-db/data.dml.sql
@@ -1,6 +1,5 @@
-insert into countries (country_name, capital, area, population, flag, map, iso_code, latitude, longitude) VALUES
-("Åland Islands", "Mariehamn", 1580, 29458, "https://flagcdn.com/w320/es.png", "https://www.openstreetmap.org/relation/1650407",  'AX', 60.116667, 19.9),
-('United Kingdom', 'London', 242900, 67215293, "https://flagcdn.com/w320/ba.png", 'https://www.openstreetmap.org/relation/62149',  'GB', 54.0, -2.0); 
 INSERT INTO countries (country_name, capital, area, population, flag, map, iso_code, latitude, longitude) VALUES
 ('Republic of Belarus','Minsk',207600,9398861,'https://flagcdn.com/w320/by.png','https://www.openstreetmap.org/relation/59065','BY',53.9,27.57),
-('Slovak Republic','Bratislava',49037,5458827,'https://flagcdn.com/w320/sk.png','https://www.openstreetmap.org/relation/14296','SK',48.15,17.12);
+('Slovak Republic','Bratislava',49037,5458827,'https://flagcdn.com/w320/sk.png','https://www.openstreetmap.org/relation/14296','SK',48.15,17.12),
+("Åland Islands", "Mariehamn", 1580, 29458, "https://flagcdn.com/w320/es.png", "https://www.openstreetmap.org/relation/1650407",  'AX', 60.116667, 19.9),
+('United Kingdom', 'London', 242900, 67215293, "https://flagcdn.com/w320/ba.png", 'https://www.openstreetmap.org/relation/62149',  'GB', 54.0, -2.0); 
