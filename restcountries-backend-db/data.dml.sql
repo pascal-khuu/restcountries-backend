f25bf5342912629823ab065f4b405d0df4e71a15
Pascal Khuu <pascal.khuu@orange.fr>
2022-04-13 14:59:48 +0200

    insertion data countries in the database restcountries

---
+++ restcountries-backend-db/data.dml.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO countries (country_name, capital, area, population, flag, map, iso_code, latitude, longitude) VALUES
+('Republic of Belarus','Minsk',207600,9398861,'https://flagcdn.com/w320/by.png','https://www.openstreetmap.org/relation/59065','BY',53.9,27.57),
+('Slovak Republic','Bratislava',49037,5458827,'https://flagcdn.com/w320/sk.png','https://www.openstreetmap.org/relation/14296','SK',48.15,17.12);
