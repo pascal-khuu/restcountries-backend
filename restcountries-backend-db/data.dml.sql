bece652d82cf124fef0729f76b50a6f57157f28f
Pascal Khuu <pascal.khuu@orange.fr>
2022-04-14 11:35:16 +0200

    insertion correction syntax of 14 European countries

--- restcountries-backend-db/data.dml.sql
+++ restcountries-backend-db/data.dml.sql
@@ -3,13 +3,13 @@ INSERT INTO countries (country_name, capital, area, population, flag, map, iso_c
 ('Slovak Republic','Bratislava',49037,5458827,'https://flagcdn.com/w320/sk.png','https://www.openstreetmap.org/relation/14296','SK', 48.66666666,19.5),
 ('Republic of Latvia','Riga',64559,1901548,'https://flagcdn.com/w320/lv.png','https://www.openstreetmap.org/relation/72594','LV',57.0,25.0),
 ('Gibraltar','Gibraltar',6,33691,'https://flagcdn.com/w320/gi.png','https://www.openstreetmap.org/relation/1278736','GI',36.13333333,-5.35),
-('Republic of Serbia','Belgrade',88361,6908224,'https://flagcdn.com/w320/rs.png','https://www.openstreetmap.org/relation/1741311','RS',44.0,21.0)
-("Åland Islands", "Mariehamn", 1580, 29458, "https://flagcdn.com/w320/es.png", "https://www.openstreetmap.org/relation/1650407",  'AX', 60.116667, 19.9),
-('United Kingdom', 'London', 242900, 67215293, "https://flagcdn.com/w320/ba.png", 'https://www.openstreetmap.org/relation/62149',  'GB', 54.0, -2.0),
-('Bosnia and Herzegovina', 'Sarajevo', 51209, 3280815, "https://flagcdn.com/w320/ba.png", 'https://www.openstreetmap.org/relation/2528142', 'BA', 44.0, 18.0),
-('Vatican City State', 'Vatican City', 0.44, 451, 'https://flagcdn.com/w320/va.png', "https://www.openstreetmap.org/relation/36989", 'VA',  41.9, 12.45),
-('Principality of Liechtenstein', 'Vaduz', 160, 38137, 'https://flagcdn.com/w320/li.png", "https://www.openstreetmap.org/relation/1155955", 'LI', 47.26666666, 9.53333333),
-('Montenegro', 'Podgorica', 13812, 621718, "https://flagcdn.com/w320/me.png", "https://www.openstreetmap.org/relation/53296", 'ME', 42.5, 19.3),
-("Republic of North Macedonia", "Skopje", 25713, "https://flagcdn.com/w320/sj.png", "https://www.openstreetmap.org/relation/53293", 'MK', 41.83333333, 22.0),
-("Kingdom of Denmark", 'Copenhagen', 43094.0, 5831404, "https://flagcdn.com/w320/dk.png", https://www.openstreetmap.org/relation/50046", 'DK', 56.0, 10.0),
-("Hellenic Republic", "Athens", 131990.0, 10715549, "https://flagcdn.com/w320/gr.png", "https://www.openstreetmap.org/relation/192307", 'GR', 39.0, 22.0);
+('Republic of Serbia','Belgrade',88361,6908224,'https://flagcdn.com/w320/rs.png','https://www.openstreetmap.org/relation/1741311','RS',44.0,21.0),
+('Aland Islands', 'Mariehamn', 1580, 29458, 'https://flagcdn.com/w320/es.png', 'https://www.openstreetmap.org/relation/1650407',  'AX', 60.116667, 19.9),
+('United Kingdom', 'London', 242900, 67215293, 'https://flagcdn.com/w320/ba.png', 'https://www.openstreetmap.org/relation/62149',  'GB', 54.0, -2.0),
+('Bosnia and Herzegovina', 'Sarajevo', 51209, 3280815, 'https://flagcdn.com/w320/ba.png', 'https://www.openstreetmap.org/relation/2528142', 'BA', 44.0, 18.0),
+('Vatican City State', 'Vatican City', 0.44, 451, 'https://flagcdn.com/w320/va.png', 'https://www.openstreetmap.org/relation/36989', 'VA',  41.9, 12.45),
+('Principality of Liechtenstein', 'Vaduz', 160, 38137, 'https://flagcdn.com/w320/li.png', 'https://www.openstreetmap.org/relation/1155955','LI', 47.26666666, 9.53333333),
+('Montenegro', 'Podgorica', 13812, 621718, 'https://flagcdn.com/w320/me.png', 'https://www.openstreetmap.org/relation/53296', 'ME', 42.5, 19.3),
+('Republic of North Macedonia', 'Skopje', 25713,2077132, 'https://flagcdn.com/w320/sj.png', 'https://www.openstreetmap.org/relation/53293', 'MK', 41.83333333, 22.0),
+('Kingdom of Denmark', 'Copenhagen', 43094.0, 5831404, 'https://flagcdn.com/w320/dk.png', 'https://www.openstreetmap.org/relation/50046', 'DK', 56.0, 10.0),
+('Hellenic Republic', 'Athens', 131990.0, 10715549, 'https://flagcdn.com/w320/gr.png', 'https://www.openstreetmap.org/relation/192307', 'GR', 39.0, 22.0);
