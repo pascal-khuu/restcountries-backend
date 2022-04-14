8403fd17c9a3ca60e04bfb65706df2997e7e789b
Pascal Khuu <pascal.khuu@orange.fr>
2022-04-14 10:45:32 +0200

    correction syntax of creation table

--- restcountries-backend-db/schema.ddl.sql
+++ restcountries-backend-db/schema.ddl.sql
@@ -1,12 +1,12 @@
 CREATE TABLE countries(
-	id SERIAL PROMARY KEY,
+	id SERIAL PRIMARY KEY,
 	country_name varchar (200) NOT NULL,
 	capital varchar (200) NOT NULL,
-	area varchar INTEGER NOT NULL,
+	area  DECIMAL NOT NULL,
 	population INTEGER NOT NULL,
 	flag varchar (255) NOT NULL,
 	map varchar (255) NOT NULL,
 	iso_code char (2) NOT NULL,
 	latitude DECIMAL NOT NULL,
-	longitude DECIMAL NOT NULL,
+	longitude DECIMAL NOT NULL
 );
